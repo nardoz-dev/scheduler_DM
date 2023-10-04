9bc772cdd08cf38220c90a4991dfd6d2bf0b0b02
nardoz-dev <nardomen96@gmail.com>
2023-10-04 18:07:33 +0200

    add gui + shell concurrency control

--- app/my_scheduler_gui.py
+++ app/my_scheduler_gui.py
@@ -2,13 +2,28 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QTextEdit, QLabel, QTextBrowser, QLineEdit, QMessageBox
 from PyQt6.QtGui import QColor
 
+class Transaction:
+    def __init__(self, name):
+        self.name = name
+        self.active = True
+        self.rollback = False
+        self.waiting = False
+
+class ResourceInfo:
+    def __init__(self,name):
+        self.name = name
+        self.rts = 0
+        self.wts = 0
+        self.wts_c = 0
+        self.cb = True
+
 class SchedulerWindow(QMainWindow):
     # Set UI 
     def __init__(self):
         super().__init__()
         # Set my Window View
         self.setWindowTitle("myScheduler")
-        self.setGeometry(100, 100, 800, 640)
+        self.setGeometry(100, 100, 910, 630)
         # Box for the scheduler
         self.scheduler_label = QLabel("Scheduler")
         # self.scheduler_text = QTextEdit()
@@ -42,6 +57,7 @@ class SchedulerWindow(QMainWindow):
         # Box for keep track the status of resources
         self.resources_status_label = QLabel("Status Resources/Transaction at the end")
         self.resources_status_text = QTextEdit()
+        self.resources_status_text.setReadOnly(True)
         # Resource/Transaction Info layout
         resources_status_layout = QVBoxLayout()
         resources_status_layout.addWidget(self.resources_status_label)
@@ -52,6 +68,7 @@ class SchedulerWindow(QMainWindow):
         # Box for the actions executed
         self.actions_label = QLabel("Workflow concurrency control")
         self.actions_text = QTextEdit()
+        self.actions_text.setReadOnly(True)
         # Layout for the actions executed
         actions_layout = QVBoxLayout()
         actions_layout.addWidget(self.actions_label)
@@ -62,9 +79,11 @@ class SchedulerWindow(QMainWindow):
         # Botton to start the concurrency throught timestamp
         self.concurrency_ts_button = QPushButton("Apply concurrency Timestamp")
         self.concurrency_ts_button.clicked.connect(self.apply_timestamp)
+        self.concurrency_ts_button.setEnabled(False)
         # Bottone for checking serializability
         self.check_serializability_button = QPushButton("Check Serializability")
         self.check_serializability_button.clicked.connect(self.check_serializability)
+        self.check_serializability_button.setEnabled(False)
         # Bottone to delete all
         self.clear_button = QPushButton("Clear All")
         self.clear_button.clicked.connect(self.clear_all)
@@ -100,14 +119,17 @@ class SchedulerWindow(QMainWindow):
         self.add_action_button.setEnabled(True)
         self.input_scheduler.setEnabled(True)
         self.generate_scheduler_button.setEnabled(True)
+        self.concurrency_ts_button.setEnabled(False)
+        self.check_serializability_button.setEnabled(False)
         # Clear Window GUI
         self.input_scheduler.clear()
         self.resources_status_text.clear()
         self.actions_text.clear()
         self.scheduler_output.clear()
         self.check_serializability_button.setStyleSheet("")
-        # Clear variable
-        # init_list()
+        # Clear variables
+        init_variable()
+    
 
     #--------------------------------------------- INPUT HANDLER
     def add_action(self):
@@ -154,13 +176,176 @@ class SchedulerWindow(QMainWindow):
         self.add_action_button.setEnabled(False)
         self.input_scheduler.setEnabled(False)
         self.generate_scheduler_button.setEnabled(False)
+        self.concurrency_ts_button.setEnabled(True)
+        self.check_serializability_button.setEnabled(True)
         self.display_scheduler()
         #init_variable_for_algorithm()
     
     #---------------------------------------- CONCURRENCY CONTROL
     def apply_timestamp(self):
-        # Each time you click on timestamp, reset the usefull list
-        pass
+        global scheduler
+
+        # Generate a list without duplicates of the needed element
+        list_resource = set([tupla[2] for tupla in scheduler if tupla[2] is not None])
+        list_transaction = set([tupla[0] for tupla in scheduler])
+        for elem in list_resource:
+            resource_info.append(ResourceInfo(elem))
+        for elem in list_transaction:
+            transaction_info.append(Transaction(elem))
+        
+        # Apply timestamp_rules over schedules
+        for elem in scheduler:
+            self.apply_rules(elem)
+
+        # Print results
+        for elem in resource_info:
+            text_to_add = vars(elem)
+            self.resources_status_text.append(str(text_to_add))
+        for elem in transaction_info:
+            text_to_add = vars(elem)
+            self.resources_status_text.append(str(text_to_add))
+
+
+    # Let's see the application of concurrency through ts.
+    def apply_rules(self, elem):  
+        global ignored_actions
+        global rollback_transaction
+        global deadlock_detector
+
+        transaction , action_type, resource = elem[0],elem[1],elem[2]
+        # Check if we need to ignore some action because transaction is in waiting or rollback
+        flag_rollback =  any(transaction in element_2 for element_2 in rollback_transaction)
+        if(any(transaction in element for element in deadlock_detector) or flag_rollback): 
+            if not flag_rollback:
+                ignored_actions.append(elem)
+        else:
+            # We get the index of the current resource and the current index of transaction of the current action in analisys
+            resource_index = None
+            for index, elem in enumerate(resource_info):
+                if elem.name == resource:
+                    resource_index = index
+                    break
+            transaction_index = None
+            for index, elem in enumerate(transaction_info):
+                if elem.name == transaction:
+                    transaction_index = index
+                    break
+
+            # Get the timestamp of the current transaction that belongs to the current action
+            transaction_ts = int(transaction[1])
+            if action_type == "write":
+                if (transaction_ts >= resource_info[resource_index].rts) and (transaction_ts  >= resource_info[resource_index].wts):
+                    if resource_info[resource_index].cb == True:
+                        resource_info[resource_index].wts = transaction_ts
+                        resource_info[resource_index].cb = False
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = OK "
+                        self.actions_text.append(text)
+                    else:
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = WAITING "
+                        self.actions_text.append(text)
+                        #add the action that generate waiting into deadlock_list_detector
+                        deadlock_detector.append(( transaction , action_type, resource ))
+                        transaction_info[transaction_index].waiting = True   
+                        ignored_actions.append(( transaction , action_type, resource ))
+
+                elif (transaction_ts >= resource_info[resource_index].rts) and (transaction_ts < resource_info[resource_index].wts):
+                    if resource_info[resource_index].cb == True:
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = IGNORE "
+                        self.actions_text.append(text)
+                    else:
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = WAITING "
+                        self.actions_text.append(text)
+                        #add the action that generate waiting into deadlock_list_detector
+                        deadlock_detector.append(( transaction , action_type, resource ))
+                        transaction_info[transaction_index].waiting = True
+                        ignored_actions.append(( transaction , action_type, resource ))   
+                else:
+                    text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = ROLLBACK "
+                    self.actions_text.append(text)
+                    #add the transaction that need to be rollbacked
+                    rollback_transaction.append(transaction)
+                    transaction_info[transaction_index].rollback = True 
+                    rollback(transaction_ts)
+
+            elif action_type == "read":
+                if (transaction_ts >= resource_info[resource_index].wts):
+                    if (resource_info[resource_index].cb == True) or (transaction_ts  == resource_info[resource_index].wts):
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = OK "
+                        self.actions_text.append(text)
+                        resource_info[resource_index].rts = max(transaction_ts, resource_info[resource_index].rts)
+                    else:
+                        text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = WAITING "
+                        self.actions_text.append(text)
+                        #add the action that generate waiting into deadlock_list_detector
+                        deadlock_detector.append(( transaction , action_type, resource ))
+                        transaction_info[transaction_index].waiting = True
+                        ignored_actions.append(elem)   
+                else:
+                    text = "Action :"+action_type+" Transaction :"+transaction+" over element :"+resource+" STATUS = ROLLBACK "
+                    self.actions_text.append(text)
+                    #add the transaction that need to be rollbacked
+                    rollback_transaction.append(transaction)
+                    transaction_info[transaction_index].rollback = True 
+                    rollback(transaction_ts)
+
+            elif action_type == "commit":
+                text = "Action :"+action_type+" Transaction :"+transaction+" STATUS = ROLLBACK "
+                self.actions_text.append(text)
+                # We can check in the array of the datainfo objects, if there's resources where the last write have the ts = to the ts of the commit, it means is the last transaction that wrote in this element,
+                # so we need to set it to true
+                resource_to_check = None
+                for index, elem in enumerate(resource_info):
+                    if elem.wts == transaction_ts :
+                        elem.cb = True
+                        elem.wts_c = transaction_ts
+                        resource_to_check = elem.name
+                # Now we need to check if some other actions are in waiting for this commit.
+                check_waiting(resource_to_check)
+
+    # function used for the transaction that are in waiting for cb("Any resource") = True
+    def check_waiting(self,resource_to_check):
+        global deadlock_detector
+        print("La risorsa che è stata trasformata in True è : ", resource_to_check)
+
+        # Remember that in order to check if the action is in waiting for the right resource we saved the action that caused waiting
+        # in this special list . (In fact is also used for check if there's deadlock)
+        #if any(resource_to_check in elem for elem in deadlock_detector): 
+
+        # Get the Transaction that is in waiting list in which the resource is set to True
+        transaction = None
+        for elem in deadlock_detector:
+            resource = elem[2]
+            if resource == resource_to_check:
+                transaction = elem[0]
+
+        if transaction is not None :
+            # Create a new list that contain all the tuple of the Transaction in the waiting list .
+            new_schedule = [tupla for tupla in ignored_actions if transaction in tupla]
+            # Remove the element in the deadlock_list because now it is processed
+            deadlock_detector = [tupla for tupla in deadlock_detector if resource_to_check not in tupla]
+            # Remove waiting status from the transaction 
+            for index, elem in enumerate(transaction_info):
+                if elem.name == transaction:
+                    elem.waiting = False
+
+            # Process the related action 
+            for elem in new_schedule:
+                #print(elem)
+                apply_rules(elem)
+                ignored_actions.remove(elem)
+
+    #Function for rollback, we need to know the ts of the transaction that generate rollback
+    #In order to use the same trick of the commit to update the value wts with wts_c i.e., the
+    #timestamp of the transaction Tj that wrote the element before the current one, and set cb(X) to true (indeed, Tj has surely committed)
+    def rollback(self,transaction_ts):
+        resource_to_check = None
+        for index, elem in enumerate(resource_info):
+            if elem.wts == transaction_ts :
+                elem.wts = elem.wts_c
+                elem.cb = True
+                resource_to_check = elem.name
+                # Now we need to check if some other transaction is in waiting , each time we put to True a new variable
+                check_waiting(resource_to_check)
 
 
     #---------------------------------------- SERIALIZABILITY
@@ -191,7 +376,6 @@ class SchedulerWindow(QMainWindow):
             self.check_serializability_button.setStyleSheet("background-color: red")
         else:
             self.check_serializability_button.setStyleSheet("background-color: green")
-    
     # Function for checking if there'are cycle (use DFS search)
     def has_cycle(self,graph):
         visited = set()
@@ -214,16 +398,30 @@ class SchedulerWindow(QMainWindow):
 
         return False
 
+
+def init_variable():
+    global scheduler
+    global ignored_actions
+    global rollback_transaction 
+    global deadlock_detector
+    global resource_info 
+    global transaction_info
+    scheduler = []
+    ignored_actions = []
+    rollback_transaction = []
+    deadlock_detector = []
+    resource_info = []
+    transaction_info = []
+
 if __name__ == '__main__':
     # Init GUI
     app = QApplication(sys.argv)
     window = SchedulerWindow()
     
     # Declare variable for algorithm workflow
-    scheduler = []
+    init_variable()
+
 
     # Show GUI
     window.show()
-
-
     sys.exit(app.exec())
